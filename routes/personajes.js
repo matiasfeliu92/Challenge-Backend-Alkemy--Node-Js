657a51e547994a44c5d514eaccb59953d8be7546
matiasfeliu92 <matumazparrote@gmail.com>
2022-05-24 17:46:39 -0300

    agregue cors al archivo de endpoints

--- routes/personajes.js
+++ routes/personajes.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const cors = require('cors');
 const control = require('../controllers/control')
 const {Personaje} = require('../database/db')
 
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 // ruta que muestra la pagina de personajes
-router.get('/', control.mostrarPers)
+router.get('/', cors(corsOptions), control.mostrarPers)
 
 // busca solamente el personaje por ID
-router.get('/:id', control.mostrarPersID)
+router.get('/:id', cors(corsOptions), control.mostrarPersID)
 
-router.post('/createCharacter', control.crearPers)
+router.post('/createCharacter', cors(corsOptions), control.crearPers)
 
-router.put('/updateCharacter/:id', control.actualizarPers)
+router.put('/updateCharacter/:id', cors(corsOptions), control.actualizarPers)
 
-router.delete('/deleteCharacter/:id', control.eliminarPers)
+router.delete('/deleteCharacter/:id', cors(corsOptions), control.eliminarPers)
 
 module.exports = router
