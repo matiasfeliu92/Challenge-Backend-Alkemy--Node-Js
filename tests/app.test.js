fb501d58086a79658d38140d0ae1d756ac54b60e
matiasfeliu92 <matumazparrote@gmail.com>
2022-05-24 17:48:08 -0300

    agregue el test que compruebe el endpoint get por id

--- tests/app.test.js
+++ tests/app.test.js
@@ -10,3 +10,13 @@ describe("GET /disney/characters", () => {
             .expect(200, done)
     })
 })
+
+describe("GET /disney/characters/:id", () => {
+    it('respond with json containing one character find by id', done => {
+        request(app)
+            .get('/disney/characters/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, done)
+    })
+})
